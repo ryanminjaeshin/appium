eb48dda68f3a783c640a2e965c7994ebc7e9b82a
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-03 22:14:12 +0900

    Preference click

--- src/test/java/ryanshin/Appium.java
+++ src/test/java/ryanshin/Appium.java
@@ -6,6 +6,7 @@ import java.net.URL;
 
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
@@ -36,6 +37,8 @@ public class Appium {
 		AndroidDriver androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"), uiAutomator2Options);
 		
 		// Actual Automation
+		// Xpath, id, className, androidUIAutomtor(AppiumBy.), accessibilityId(AppiumBy.)
+		androidDriver.findElement(AppiumBy.accessibilityId("Preference")).click();
 		
 		
 		androidDriver.quit();
